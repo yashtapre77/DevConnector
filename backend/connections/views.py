75832753a045154cfef1c2304dbc4e0375145da1
yashtapre77 <yashtapre77@gmail.com>
2025-01-29 21:52:11 +0530

    required imports for views

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.conf import settings
+import requests
+
+from .serializers import (
+    UserSerializer,
+    ProfileSerializer,
+    GetProfileSerializer,
+    ExperienceSerializer,
+    EducationSerializer,
+    PostSerializer,
+    GetPostSerializer,
+    CommentSerializer
+)
+from .models import (
+    User,
+    Profile,
+    Experience,
+    Education,
+    Post,
+    Comment
+)
+
 # Create your views here.
