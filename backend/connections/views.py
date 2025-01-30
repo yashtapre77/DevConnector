777c44197ad47c5d5c295d6ed9ab69aa2d0d570e
yashtapre77 <yashtapre77@gmail.com>
2025-01-30 21:52:11 +0530

    created user view for authentication

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -43,3 +43,29 @@ class UserView(APIView):
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+
+class GetAuthUserView(APIView):
+    authentication_classes = (TokenAuthentication,)
+
+    def get(self, request):
+        token = request.headers.get('Authorization')
+        if not token:
+            return Response(data={'error':'No Token. Authorization Denied'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = User.objects.get(id=request.user.id)
+        data = UserSerializer(user).data
+        return Response(data)
+        
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if email == "" or password == "":
+            return Response({'error': 'Please provide both email and password'},status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(username=email, password=password)
+
+        if not user:
+            return Response({'error': 'Invalid Credentials'},status=status.HTTP_404_NOT_FOUND)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
