3dba6650ae44388946175f9183fc8cc0ec535a32
yashtapre77 <yashtapre77@gmail.com>
2025-02-06 21:52:11 +0530

    added login page design

---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>      
+    </div>
+  )
+}
+
+export default Login
