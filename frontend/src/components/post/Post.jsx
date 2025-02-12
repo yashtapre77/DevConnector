7cd6b84775bf3dab21de157b40d473a67c59e2d9
yashtapre77 <yashtapre77@gmail.com>
2025-02-12 21:52:11 +0530

    added functionality in post

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import { getPost } from "../../actions/post";
+import PostItem from "../posts/PostItem";
+import CommentForm from "../post/CommentForm";
+import CommentItem from "../post/CommentItem";
 
 function Post() {
-  return (
+  const { post, loading } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPost(match.params.id));
+  }, [dispatch, match.params.id]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
     <div>
-       <Link to="/posts" className="btn">
+      <Link to="/posts" className="btn">
         Back To Posts
       </Link>
 
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post.id} />
       <div className="comments">
-        {post.post_comments.map(comment => (
+        {post.post_comments.map((comment) => (
           <CommentItem key={comment.id} comment={comment} postId={post.id} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
