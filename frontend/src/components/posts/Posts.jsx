cdc292701f76b11d7b7ed8412232e14218b925ee
yashtapre77 <yashtapre77@gmail.com>
2025-02-15 21:52:11 +0530

    added posts page functionality

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
+import { getPosts } from "../../actions/post";
+import PostForm from "./PostForm";
 
 function Posts() {
-  return (
+  const { posts, loading } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return loading ? (
+    <Spinner />
+  ) : (
     <div>
-       <h1 className="large text-primary">Posts</h1>
+      <h1 className="large text-primary">Posts</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Welcome to the community!
       </p>
       <PostForm />
-      {posts.map(post => (
+      {posts.map((post) => (
         <PostItem key={post.id} post={post} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
