ea8814e735a194bc2ddd4fa08a8cc7cad5be1872
yashtapre77 <yashtapre77@gmail.com>
2025-02-18 22:52:11 +0530

    started  profile about page design

---
+++ frontend/src/components/profile/ProfileAbout.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function ProfileAbout() {
+  return (
+    <div>
+      <div className="profile-about bg-light p-2">
+      {bio && (
+        <Fragment>
+          <h2 className="text-primary">{name.trim().split(" ")[0]}'s Bio</h2>
+          <p>{bio}</p>
+        </Fragment>
+      )}
+      <div className="line"></div>
+      <h2 className="text-primary">Skill Set</h2>
+      <div className="skills">
+        {skills
+          .trim()
+          .split(",")
+          .map((skill, i) => (
+            <div className="p-1" key={i}>
+              <i className="fa fa-check"></i>
+              {skill}
+            </div>
+          ))}
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default ProfileAbout
