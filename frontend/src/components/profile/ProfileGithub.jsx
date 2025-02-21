fe3365f1807286437a60214040b2075ab9f44883
yashtapre77 <yashtapre77@gmail.com>
2025-02-21 22:52:11 +0530

    started github profile page design

---
+++ frontend/src/components/profile/ProfileGithub.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function ProfileGithub() {
+
+    
+  return (
+    <div>
+      <div className="profile-github">
+      <h2 className="text-primary my-1">
+        <i className="fab fa-github"></i> Github Repos
+      </h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map(repo => (
+          <div className="repo bg-white p-1 my-1" key={repo.id}>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className="badge badge-primary">
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className="badge badge-dark">
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className="badge badge-light">Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+    </div>
+  )
+}
+
+export default ProfileGithub
