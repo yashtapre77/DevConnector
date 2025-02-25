16422b2b45ba1247081189474b9ee722bc77c157
yashtapre77 <yashtapre77@gmail.com>
2025-02-25 23:58:11 +0530

    created add education page

---
+++ frontend/src/components/profile-forms/AddEducation.jsx
@@ -0,0 +1,95 @@
+    import React from 'react'
+    
+    function AddEducation() {
+      return (
+        <div>
+          <h1 className="large text-primary">Add Your Education</h1>
+      <p className="lead">
+        <i className="fas fa-graduation-cap"></i> Add any school, bootcamp, etc
+        that you have attended
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={onSubmit} noValidate>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* School or Bootcamp"
+            name="school"
+            value={school}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree or Certificate"
+            name="degree"
+            value={degree}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Field Of Study"
+            name="field_of_study"
+            value={field_of_study}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input
+            type="date"
+            name="from_date"
+            value={from_date}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              onChange={e => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+            />{" "}
+            Current School or Bootcamp
+          </p>
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            name="to_date"
+            value={to_date}
+            onChange={onChange}
+            disabled={toDateDisabled ? "disabled" : ""}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Program Description"
+            value={description}
+            onChange={onChange}
+          ></textarea>
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Go Back
+        </Link>
+      </form>
+        </div>
+      )
+    }
+    
+    export default AddEducation
+    
