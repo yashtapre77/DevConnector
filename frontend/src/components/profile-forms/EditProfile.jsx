a3590d0832b2e7836f7233efa1206741ebb6414a
yashtapre77 <yashtapre77@gmail.com>
2025-02-28 23:58:11 +0530

    select in Edit Profile page

--- frontend/src/components/profile-forms/EditProfile.jsx
+++ frontend/src/components/profile-forms/EditProfile.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { createProfile, getCurrentProfile } from "../../actions/profile";
 
 function EditProfile() {
     const profile = useSelector(state => state.profile.profile);
@@ -50,7 +53,31 @@ function EditProfile() {
     };
   return (
     <div>
-      
+         <h1 className="large text-primary">Create Your Profile</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Let's get some information to make your
+        profile stand out
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <select name="status" value={status} onChange={onChange}>
+            <option value="0">* Select Professional Status</option>
+            <option value="Developer">Developer</option>
+            <option value="Junior Developer">Junior Developer</option>
+            <option value="Senior Developer">Senior Developer</option>
+            <option value="Manager">Manager</option>
+            <option value="Student or Learning">Student or Learning</option>
+            <option value="Instructor">Instructor or Teacher</option>
+            <option value="Intern">Intern</option>
+            <option value="Other">Other</option>
+          </select>
+          <small className="form-text">
+            Give us an idea of where you are at in your career
+          </small>
+          </div>
+          </form>
+
     </div>
   )
 }
