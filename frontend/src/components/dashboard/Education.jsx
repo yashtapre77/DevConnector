2bce71898609217030f8ceabe9bdec0f6895cf85
yashtapre77 <yashtapre77@gmail.com>
2025-03-02 22:58:11 +0530

    Copleted Education.jsx

--- frontend/src/components/dashboard/Education.jsx
+++ frontend/src/components/dashboard/Education.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import Moment from "react-moment";
+import { useDispatch } from "react-redux";
+import { deleteEducation } from "../../actions/profile";
 
-function Education() {
+function Education({ education }) {
+  const dispatch = useDispatch();
+  const educations =
+    education &&
+    education.map((edu) => (
+      <tr key={edu.id}>
+        <td>{edu.school}</td>
+        <td className="hide-sm">{edu.degree}</td>
+        <td className="hide-sm">
+          <Moment format="YYYY/MM/DD">{edu.from_date}</Moment> -{" "}
+          {edu.to_date == null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.to_date}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => dispatch(deleteEducation(edu.id))}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
   return (
     <div>
-       <h2 className="my-2">Education Credentials</h2>
+      <h2 className="my-2">Education Credentials</h2>
       <table className="table">
         <thead>
           <tr>
@@ -16,7 +44,7 @@ function Education() {
         <tbody>{educations}</tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default Education
+export default Education;
