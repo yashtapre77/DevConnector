eb5008c71ebae4083024dec37281dfe1ad3fe2a8
yashtapre77 <yashtapre77@gmail.com>
2025-03-02 21:58:11 +0530

    Education in dashboard

---
+++ frontend/src/components/dashboard/Education.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Education() {
+  return (
+    <div>
+       <h2 className="my-2">Education Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>School</th>
+            <th className="hide-sm">Degree</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Education
