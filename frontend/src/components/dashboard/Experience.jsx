7bfe62192287a6538d7bb0f8c443ce2819969da0
yashtapre77 <yashtapre77@gmail.com>
2025-03-03 22:58:11 +0530

    Experience functionlity in dashboard

--- frontend/src/components/dashboard/Experience.jsx
+++ frontend/src/components/dashboard/Experience.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import Moment from "react-moment";
+import { useDispatch } from "react-redux";
+import { deleteExperience } from "../../actions/profile";
 
-function Experience() {
+function Experience({experience}) {
+    const dispatch = useDispatch();
+
+    const experiences = experience.map(exp => (
+      <tr key={exp.id}>
+        <td>{exp.company}</td>
+        <td className="hide-sm">{exp.title}</td>
+        <td className="hide-sm">
+          <Moment format="YYYY/MM/DD">{exp.from_date}</Moment> -{" "}
+          {exp.to_date == null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to_date}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => dispatch(deleteExperience(exp.id))}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
   return (
     <div>
       <h2 className="my-2">Experience Credentials</h2>
