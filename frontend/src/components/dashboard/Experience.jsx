78d1da1df87e57188892b599204a203ad38b17b0
yashtapre77 <yashtapre77@gmail.com>
2025-03-03 21:58:11 +0530

    Experience design in dashboard

---
+++ frontend/src/components/dashboard/Experience.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div>
+      <h2 className="my-2">Experience Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className="hide-sm">Title</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{experiences}</tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Experience
