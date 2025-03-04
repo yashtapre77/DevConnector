ecf5045999b44558c0c50130ba700e8ad75e0f14
yashtapre77 <yashtapre77@gmail.com>
2025-03-04 21:58:11 +0530

    added  dashboard design

---
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+       <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
+
+          <div className="my-2">
+            <button
+              className="btn btn-danger"
+              onClick={() => dispatch(deleteAccount())}
+            >
+              <i className="fas fa-user-minus"></i> Delete My Account
+            </button>
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </div>
+  )
+}
+
+export default Dashboard
